7f743bc0594a31be8bd3d97369555c6d003aa4e4
tahrlwf5 <109966236+tahrlwf5@users.noreply.github.com>
2025-02-28 16:00:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,17 +4,18 @@ import PyPDF2
 from googletrans import Translator
 from dotenv import load_dotenv
 import os
+from reportlab.pdfgen import canvas
 
-# تحميل متغيرات البيئة من ملف .env
 load_dotenv()
 
-# استبدل 'YOUR_TELEGRAM_BOT_TOKEN' برمز البوت الخاص بك
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-# إنشاء كائن البوت
+if TELEGRAM_BOT_TOKEN is None:
+    print("Error: TELEGRAM_BOT_TOKEN is not set. Please check your .env file or Docker environment variables.")
+    exit(1)
+
 bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
 
-# دالة لاستخراج النص من ملف PDF
 def extract_text_from_pdf(file_path):
     text = ""
     with open(file_path, 'rb') as file:
@@ -23,17 +24,21 @@ def extract_text_from_pdf(file_path):
             text += page.extract_text()
     return text
 
-# دالة لترجمة النص
 def translate_text(text):
     translator = Translator()
     translation = translator.translate(text, src='en', dest='ar')
     return translation.text
 
-# معالج لأمر /start
+def create_translated_pdf(original_file_name, translated_text):
+    translated_file_name = f"translated_{original_file_name}"
+    c = canvas.Canvas(translated_file_name)
+    c.drawString(100, 750, translated_text)  # يمكنك تعديل الموضع والتنسيق هنا
+    c.save()
+    return translated_file_name
+
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="أرسل لي ملف PDF لترجمته.")
 
-# معالج لرسائل الملفات
 def handle_document(update, context):
     file = context.bot.get_file(update.message.document.file_id)
     file_path = f'{update.message.document.file_name}'
@@ -42,20 +47,20 @@ def handle_document(update, context):
     try:
         text = extract_text_from_pdf(file_path)
         translated_text = translate_text(text)
-        context.bot.send_message(chat_id=update.effective_chat.id, text=translated_text)
+        translated_file_name = create_translated_pdf(update.message.document.file_name, translated_text)
+        context.bot.send_document(chat_id=update.effective_chat.id, document=open(translated_file_name, 'rb'))
     except Exception as e:
         context.bot.send_message(chat_id=update.effective_chat.id, text=f"حدث خطأ: {e}")
     finally:
-         os.remove(file_path)
+        os.remove(file_path)
+        if 'translated_file_name' in locals() and os.path.exists(translated_file_name):
+            os.remove(translated_file_name)
 
-# إنشاء الكائن Updater
 updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
-# إضافة المعالجات
 dispatcher.add_handler(CommandHandler('start', start))
 dispatcher.add_handler(MessageHandler(Filters.document, handle_document))
 
-# بدء تشغيل البوت
 updater.start_polling()
 updater.idle()
